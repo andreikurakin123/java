20db0820297a1d2e11e66eb2ee017a56ebe761f1
andreikurakin <andreikurakin@gmail.com>
2025-11-26 19:01:58 +0300

    task_4_3

---
+++ Operators and Data types/task_4/task_4_3/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Integer integerObject = null; // Объект Integer инициализирован значением null
+        
+        try {
+            // Попытка автораспаковки, будет выброшено исключение NullPointerException
+            @SuppressWarnings("null")
+            int primitiveInt = integerObject; // Автораспаковка
+            System.out.println("Значение primitiveInt: " + primitiveInt);
+        } catch (NullPointerException e) {
+            System.out.println("Исключение: " + e);
+        }
+    }
+
+}
