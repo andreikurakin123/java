ad320bfc6979fafcf3a686bd25301fcf6382bde8
andreikurakin <andreikurakin@gmail.com>
2025-11-26 17:21:11 +0300

    task_1_7

---
+++ Operators and Data types/task_1/task_1_7/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] arg) {
+        // answer.md
+        
+         
+        int a = 120;
+        byte b = a + 10;
+        byte c = (byte)(a + 10);
+        byte d = a + 1;
+
+    }
+}
