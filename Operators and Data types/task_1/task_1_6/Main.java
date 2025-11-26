d940cdc19bfe4288676a955c779673c072c64781
andreikurakin <andreikurakin@gmail.com>
2025-11-26 17:20:57 +0300

    task_1_6

---
+++ Operators and Data types/task_1/task_1_6/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Для сужения нужно явное указание (уменьшаемый тип) value
+
+        double doubleValue = 9.99;
+        // Преобразование double в int
+
+        int intValue = (int) doubleValue; // явное преобразование
+        System.out.println("Преобразование double в int: " + intValue); // Вывод: 9
+    }
+}
