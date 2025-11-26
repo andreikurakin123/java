ee2f58da9f3dc23d400709059e61113186df43eb
andreikurakin <andreikurakin@gmail.com>
2025-11-26 17:13:26 +0300

    task_1_3

---
+++ Operators and Data types/task_1/task_1_2/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /* 
+         * \u0410 — это символ "А"
+         * \u043D — это символ "н" 
+         * \u0434 — это символ "д"
+         * \u0440 — это символ "р"
+         * \u0435 — это символ "е"
+         * \u0439 — это символ "й"
+         */
+        String name = "\u0410\u043D\u0434\u0440\u0435\u0439"; // Андрей
+        System.out.println(name);
+    }
+}
