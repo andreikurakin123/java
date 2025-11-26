0ba1283d3782a4eac9267cdef9cf6940f0a837b7
andreikurakin <andreikurakin@gmail.com>
2025-11-26 17:14:22 +0300

    task_1_3

---
+++ Operators and Data types/task_1/task_1_3/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] arg) {
+        //answer.md
+
+        int b;
+
+        int a = 1;
+        {
+            b = 2;
+        }
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
