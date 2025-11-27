92e0c3d8d17c67cc61de2e1291a8c6be02e82427
andreikurakin <andreikurakin@gmail.com>
2025-11-27 17:19:58 +0300

    task_7_1

---
+++ Operators and Data types/task_7/task_7_1/Main.java
@@ -0,0 +1,15 @@
+int main() {
+    // Бесконечный цикл while
+    cout << "Бесконечный цикл while:" << endl;
+    while (true) {
+        cout << "Этот текст будет повторяться вечно!" << endl;
+    }
+    
+    // Бесконечный цикл do-while
+    cout << "\nБесконечный цикл do-while:" << endl;
+    do {
+        cout << "И этот текст тоже!" << endl;
+    } while (true);
+    
+    return 0;
+}
