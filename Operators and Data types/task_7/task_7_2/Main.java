c5716b2de4a0775d2a8bf37ac850f52aa908c46f
andreikurakin <andreikurakin@gmail.com>
2025-11-27 17:20:10 +0300

    task_7_2

---
+++ Operators and Data types/task_7/task_7_2/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] arg) {
+        double result = 0;
+        int counter = 0;
+
+        while ((++counter) > 0) {
+            if (counter % 2 != 0) {
+                continue;
+            }
+
+            result += counter;
+
+            if (counter == 1234567890) {
+                break;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
