2b41e4f681ee98328cb2570374259e89c3661de3
andreikurakin <andreikurakin@gmail.com>
2025-11-27 17:19:16 +0300

    task_5

---
+++ Operators and Data types/task_5/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] arg) {
+        int a = 2;
+        int i = (a == 1) ? 1 : ((a == 2) ? 2 : 3);
+        
+        System.out.println(i);
+    }
+}
