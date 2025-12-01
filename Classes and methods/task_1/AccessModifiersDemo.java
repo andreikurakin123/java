6a7a7f92ec07de1c19052209960b7e72f2423432
andreikurakin <andreikurakin@gmail.com>
2025-12-01 16:16:47 +0300

    task_1

---
+++ Classes and methods/task_1/AccessModifiersDemo.java
@@ -0,0 +1,22 @@
+public class AccessModifiersDemo {
+    public String publicVar = "public";
+    private String privateVar = "private";
+    protected String protectedVar = "protected";
+    String packagePrivateVar = "package-private";
+
+    public void displayPublic() {
+        System.out.println(publicVar);
+    }
+    
+    @SuppressWarnings("unused")
+    private void displayPrivate() { // Чисто чтобы затестить недоступность
+        System.out.println(privateVar);
+    }
+    
+    protected void displayProtected() {
+        System.out.println(protectedVar);
+    }
+    
+    void displayPackagePrivate() {
+        System.out.println(packagePrivateVar);
+   
