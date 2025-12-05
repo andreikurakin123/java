636079e5daa28e6e232aa62dd2e626c33b55c73b
andreikurakin <andreikurakin@gmail.com>
2025-12-06 01:23:34 +0300

    task_6

---
+++ Exceptions and errors/task_6/App.java
@@ -0,0 +1,11 @@
+public class App {
+    // answer.md
+    final static int START_COUNTER;
+    static {
+        START_COUNTER = Integer.parseInt("Y-");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
