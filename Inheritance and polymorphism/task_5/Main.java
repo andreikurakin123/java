7bed9634adebe1791c942fa19b6293550c2ae2a1
andreikurakin <andreikurakin@gmail.com>
2025-12-06 00:19:21 +0300

    task_5

---
+++ Inheritance and polymorphism/task_5/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    //answer.md
+    
+    class A {
+        public static void method() {
+            System.out.println("A's static method");
+        }
+    }
+
+    class B extends A {
+        public static void method() {
+            System.out.println("B's static method");
+        }
+    }
+
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        Main main = new Main();
+        A obj = main.new B();
+        obj.method(); // Выведет: "A's static method"
+    }
+}
