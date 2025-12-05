5fe3a104d364d5e214d773ad6c6d4075139f7d95
andreikurakin <andreikurakin@gmail.com>
2025-12-06 01:05:25 +0300

    task_3_2

---
+++ Exceptions and errors/task_3/task_3_2/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int result = divide(10, 0);
+            System.out.println("Результат деления: " + result);
+        } catch (final ArithmeticException e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        }
+
+        System.out.println("Программа продолжает работу после обработки исключения.");
+    }
+}
