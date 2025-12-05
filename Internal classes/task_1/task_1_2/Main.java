13387f7333503bbe9f41ffdf0abe4683e7272f9d
andreikurakin <andreikurakin@gmail.com>
2025-12-06 00:19:44 +0300

    task_1_2

---
+++ Internal classes/task_1/task_1_2/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    private String privateField = "I'm private!";
+    public String publicField = "I'm public!";
+
+    private void privateMethod() {
+        System.out.println("Приватный метод в Main.");
+    }
+
+    public void publicMethod() {
+        System.out.println("Публичный метод в Main.");
+    }
+
+    class InnerClass {
+        public void accessOuter() {
+            System.out.println(privateField);
+            System.out.println(publicField); 
+            
+            privateMethod(); 
+            publicMethod();  
+        }
+    }
+
+    public static void main(String[] args) {
+        Main mainInstance = new Main();
+        InnerClass innerInstance = mainInstance.new InnerClass();
+        innerInstance.accessOuter();
+    }
+}
