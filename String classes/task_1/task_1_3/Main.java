d07171da827870fa5232e0ec3c126ee3e5c2862b
andreikurakin <andreikurakin@gmail.com>
2025-12-06 00:46:44 +0300

    task_1_3

---
+++ String classes/task_1/task_1_3/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] arg) {
+        String multiLineString = """
+       длиииииииииииииииииииииииииииииииииии
+       иииииииииииинная строка вот такая 
+       большая вот так вот вот 
+       """;
+       
+       System.out.println(multiLineString);
+    }
+}
